f5592f2f42c1df9c378a555263562bd76736f013
KathrineSN <KathrineSN@live.dk>
2022-03-09 14:37:05 +0100

    added acc metrics

--- fit.py
+++ fit.py
@@ -1,6 +1,7 @@
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import KFold
 import pandas as pd
+import numpy as np
 from pelutils import log
 
 
@@ -15,4 +16,15 @@ def fit(df: pd.DataFrame, num_splits: int):
         x_test, y_test = test_df.drop("LoadFactor", axis=1), test_df.LoadFactor.values
         model = RandomForestRegressor(n_estimators=10)
         model.fit(x_train, y_train)
-        model.predict(x_test)
+        y_pred = model.predict(x_test)
+        
+        # Deviation per flight
+        n_pas_pred = np.ceil(y_pred * x_test['SeatCapacity']) # number of predicted passengers
+        n_pas = np.ceil(y_test * x_test['SeatCapacity']) # actual number of passengers
+        
+        diff = (n_pas - n_pas_pred)
+        dev_per_fli = np.divide(diff, n_pas, out=np.zeros_like(diff), where=n_pas!=0) # Deviating per flight
+        acc_per_fli = np.ones(len(dev_per_fli))- abs(dev_per_fli) # Accuracy per flight
+        acc = np.mean(acc_per_fli) # overall accuracy
+        
+
--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from pelutils import log, Levels
 
 
 if __name__ == "__main__":
-    log.configure("case1.log", print_level=Levels.DEBUG)
+    #log.configure("case1.log", print_level=Levels.DEBUG)
     with log.log_errors:
         df = load("Realized Schedule 20210101-20220208.xlsx")
         fit(df, 5)
